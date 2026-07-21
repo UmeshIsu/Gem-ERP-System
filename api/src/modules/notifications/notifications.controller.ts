5bead211f371883b1eaaff888d2c1aeab880cd3e
dulaj yuthsara <dulajyuthsara22@gmail.com>
2026-07-21 10:08:32 +0530

    bug fixes

--- api/src/modules/notifications/notifications.controller.ts
+++ api/src/modules/notifications/notifications.controller.ts
@@ -1,27 +1,43 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post, Query } from '@nestjs/common';
-import { NotificationsService } from './notifications.service';
-import { AuthUser, CurrentUser } from '../../common/decorators/current-user.decorator';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Query,
+} from "@nestjs/common";
+import { NotificationsService } from "./notifications.service";
+import {
+  AuthUser,
+  CurrentUser,
+} from "../../common/decorators/current-user.decorator";
 
-@Controller('notifications')
+@Controller("notifications")
 export class NotificationsController {
   constructor(private notifications: NotificationsService) {}
 
   @Get()
-  list(@CurrentUser() user: AuthUser, @Query('unreadOnly') unreadOnly?: string) {
-    return this.notifications.listForUser(user.id, unreadOnly === 'true');
+  list(
+    @CurrentUser() user: AuthUser,
+    @Query("unreadOnly") unreadOnly?: string,
+  ) {
+    return this.notifications.listForUser(user.id, unreadOnly === "true");
   }
 
-  @Get('unread-count')
+  @Get("unread-count")
   unreadCount(@CurrentUser() user: AuthUser) {
     return this.notifications.unreadCount(user.id);
   }
 
-  @Post(':id/read')
-  markRead(@Param('id', ParseUUIDPipe) id: string) {
-    return this.notifications.markRead(id);
+  @Post(":id/read")
+  markRead(
+    @Param("id", ParseUUIDPipe) id: string,
+    @CurrentUser() user: AuthUser,
+  ) {
+    return this.notifications.markRead(id, user.id);
   }
 
-  @Post('read-all')
+  @Post("read-all")
   markAllRead(@CurrentUser() user: AuthUser) {
     return this.notifications.markAllRead(user.id);
   }
